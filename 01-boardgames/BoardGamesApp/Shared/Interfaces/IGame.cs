c613d7b435aeb52e686114513aa3043ab776ad39
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-03-04 22:26:56 +0100

    refactor: remove commented out code in IGame.cs

--- 01-boardgames/BoardGamesApp/Shared/Interfaces/IGame.cs
+++ 01-boardgames/BoardGamesApp/Shared/Interfaces/IGame.cs
@@ -5,7 +5,5 @@ public interface IGame
     void Play();
     bool IsGameOver();
     void Restart();
-    //GameBoard GetBoard();
-    // IGameEvaluator GetEvaluator();
     PlayerSymbol GetCurrentPlayerSymbol();
 }
