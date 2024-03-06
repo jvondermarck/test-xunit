c511947b331e3bfb2b972f07a6b5a10804c43de0
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-03-06 20:39:57 +0100

    test(): add TicTacToeComputerPlayerTests

---
+++ 01-boardgames/BoardGamesApp.Tests/TicTacToeComputerPlayerTests.cs
@@ -0,0 +1,22 @@
+﻿namespace BoardGamesApp.Tests;
+
+public class TicTacToeComputerPlayerTests
+{
+    [Fact]
+    public void TicTacToeComputerPlayer_MakeMove_ShouldMakeValidMove()
+    {
+        // Arrange
+        GameBoard gameBoard = new GameBoard(3, 3);
+        TicTacToeComputerPlayer computerPlayer = new TicTacToeComputerPlayer(PlayerSymbol.X);
+
+        // Act
+        computerPlayer.MakeMove(gameBoard);
+
+        // Assert that the computer player made a valid move
+        Assert.All(gameBoard.board.Cast<char>(), cell => {
+            if (cell != 'X') {
+                Assert.Equal(' ', cell);
+            }
+        });
+    }
+}
