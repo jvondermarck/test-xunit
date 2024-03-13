a22e912d4b1c9ef8385076956219b3b595783179
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-03-13 16:25:04 +0100

    refactor: DisplayScore method to use ScoreToString helper method

--- 02-tdd-tennis/TennisApp/Tennis.cs
+++ 02-tdd-tennis/TennisApp/Tennis.cs
@@ -7,20 +7,33 @@
                 
         }
 
-
+        private static string ScoreToString(int score)
+        {
+            switch (score)
+            {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+                case 4:
+                    return "Advantage";
+                default:
+                    return "Invalid score";
+            }
+        }
         
         public static string DisplayScore(int player1, int player2)
         {
-            if(player1 == 1 && player2 == 0)
-            {
-                return "Fifteen-Love";
-            } 
-            else if(player1 == 2 && player2 == 0)
-            {
-                return "Thirty-Love";
-            }
+            string scorePlayer1 = ScoreToString(player1);
+            string scorePlayer2 = ScoreToString(player2);
+            
+            
 
-            return "";
+            return scorePlayer1 + "-" + scorePlayer2;
         }
     }
 }
--- 02-tdd-tennis/TennisAppTests/TestTennis.cs
+++ 02-tdd-tennis/TennisAppTests/TestTennis.cs
@@ -7,6 +7,14 @@ namespace TennisAppTests
         [Theory]
         [InlineData(1, 0, "Fifteen-Love")]
         [InlineData(2, 0, "Thirty-Love")]
+        [InlineData(3, 0, "Forty-Love")]
+        [InlineData(0, 1, "Love-Fifteen")]
+        [InlineData(0, 2, "Love-Thirty")]
+        [InlineData(0, 3, "Love-Forty")]
+        [InlineData(1, 1, "Fifteen-Fifteen")]
+        [InlineData(2, 2, "Thirty-Thirty")]
+        [InlineData(3, 3, "Forty-Forty")]
+        [InlineData(4, 4, "Advantage-Advantage")]
         public void Test_DisplayScore(int scorePlayer1, int scorePlayer2, string expectedResult)
         {
             string displayedScore = Tennis.DisplayScore(scorePlayer1, scorePlayer2);
