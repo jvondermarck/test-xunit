41349012948a9a49638a1fea666723f51515b65a
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-03-16 16:22:04 +0100

    test: add Loan class to LoanManagementApp

---
+++ 03-loan/LoanManagementApp/Loan.cs
@@ -0,0 +1,30 @@
+﻿namespace LoanManagementApp;
+
+public class Loan
+{
+    private double _principal;
+    private int _termInMonths;
+    private double _annualInterestRate;
+
+    public Loan(double principal, int termInMonths, double annualInterestRate)
+    {
+        _principal = principal;
+        _termInMonths = termInMonths;
+        _annualInterestRate = annualInterestRate;
+    }
+
+    public double GetPrincipal()
+    {
+        return _principal;
+    }
+
+    public int GetTermInMonths()
+    {
+        return _termInMonths;
+    }
+
+    public double GetAnnualInterestRate()
+    {
+        return _annualInterestRate;
+    }
+}
---
+++ 03-loan/LoanManagementAppTests/LoanTest.cs
@@ -0,0 +1,45 @@
+﻿using LoanManagementApp;
+
+namespace LoanManagementAppTests;
+
+public class LoanTest
+{
+    [Fact]
+    public void GetPrincipal_ValidPrincipal_ReturnsPrincipal()
+    {
+        // Arrange
+        var loan = new Loan(1000, 12, 0.1);
+        
+        // Act
+        var principal = loan.GetPrincipal();
+        
+        // Assert
+        Assert.Equal(1000, principal);
+    }
+    
+    [Fact]
+    public void GetTermInMonths_ValidTerm_ReturnsTerm()
+    {
+        // Arrange
+        var loan = new Loan(1000, 12, 0.1);
+        
+        // Act
+        var term = loan.GetTermInMonths();
+        
+        // Assert
+        Assert.Equal(12, term);
+    }
+    
+    [Fact]
+    public void GetAnnualInterestRate_ValidInterestRate_ReturnsInterestRate()
+    {
+        // Arrange
+        var loan = new Loan(1000, 12, 0.1);
+        
+        // Act
+        var interestRate = loan.GetAnnualInterestRate();
+        
+        // Assert
+        Assert.Equal(0.1, interestRate);
+    }
+}
