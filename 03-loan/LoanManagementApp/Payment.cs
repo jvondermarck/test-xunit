6627aca1b3a82a53108becb61da1b2d7f3f861f7
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-03-16 17:06:43 +0100

    tes: add payment class

---
+++ 03-loan/LoanManagementApp/Payment.cs
@@ -0,0 +1,30 @@
+﻿namespace LoanManagementApp;
+
+public class Payment
+{
+    private int _paymentNumber;
+    private double _principalPaid;
+    private double _remainingBalance;
+
+    public Payment(int paymentNumber, double principalPaid, double remainingBalance)
+    {
+        _paymentNumber = paymentNumber;
+        _principalPaid = principalPaid;
+        _remainingBalance = remainingBalance;
+    }
+
+    public int GetPaymentNumber()
+    {
+        return _paymentNumber;
+    }
+
+    public double GetPrincipalPaid()
+    {
+        return _principalPaid;
+    }
+
+    public double GetRemainingBalance()
+    {
+        return _remainingBalance;
+    }
+}
---
+++ 03-loan/LoanManagementAppTests/PaymentTest.cs
@@ -0,0 +1,20 @@
+﻿using LoanManagementApp;
+namespace LoanManagementAppTests;
+
+public class PaymentTest
+{
+    [Fact]
+    public void Test_Payment()
+    {
+        // Arrange
+        int paymentNumber = 1;
+        double principalPaid = 500;
+        double remainingBalance = 1500;
+        Payment payment = new Payment(paymentNumber, principalPaid, remainingBalance);
+
+        // Act & Assert
+        Assert.Equal(paymentNumber, payment.GetPaymentNumber());
+        Assert.Equal(principalPaid, payment.GetPrincipalPaid());
+        Assert.Equal(remainingBalance, payment.GetRemainingBalance());
+    }
+}
