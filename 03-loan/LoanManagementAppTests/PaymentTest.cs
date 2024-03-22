6f64f5e2c6fe2062d93f8292923872d763d5971b
Julien Von Der Marck <vdmjulien@gmail.com>
2024-03-22 10:41:17 +0100

    test(PaymentTest): refactor class to include additional test methods

--- 03-loan/LoanManagementAppTests/PaymentTest.cs
+++ 03-loan/LoanManagementAppTests/PaymentTest.cs
@@ -4,7 +4,7 @@ namespace LoanManagementAppTests;
 public class PaymentTest
 {
     [Fact]
-    public void Test_Payment()
+    public void Test_Payment_GetPaymentNumber()
     {
         // Arrange
         int paymentNumber = 1;
@@ -14,7 +14,33 @@ public class PaymentTest
 
         // Act & Assert
         Assert.Equal(paymentNumber, payment.GetPaymentNumber());
+    }
+
+    [Fact]
+    public void Test_Payment_GetPrincipalPaid()
+    {
+        // Arrange
+        int paymentNumber = 1;
+        decimal principalPaid = 500;
+        decimal remainingBalance = 1500;
+        Payment payment = new Payment(paymentNumber, principalPaid, remainingBalance);
+
+        // Act & Assert
         Assert.Equal(principalPaid, payment.GetPrincipalPaid());
+    }
+
+    [Fact]
+    public void Test_Payment_GetRemainingBalance()
+    {
+        // Arrange
+        int paymentNumber = 1;
+        decimal principalPaid = 500;
+        decimal remainingBalance = 1500;
+        Payment payment = new Payment(paymentNumber, principalPaid, remainingBalance);
+
+        // Act & Assert
         Assert.Equal(remainingBalance, payment.GetRemainingBalance());
     }
+
+
 }
